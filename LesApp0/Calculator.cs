f40a4638c71615736adf2b70eb0d3f9c0515fc07
Bogdan <pinchuk.brus@gmail.com>
2019-08-05 12:59:05 +0300

    Додано LesApp0

---
+++ LesApp0/Calculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp0
+{
+    /// <summary>
+    /// Калькулятор
+    /// </summary>
+    static class Calculator
+    {
+        /// <summary>
+        /// Сума
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static void Add(double a, double b) => Console.WriteLine("\n\t" + a + " + " + b + " = " + (a + b));
+        /// <summary>
+        /// Різниця
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static void Sub(double a, double b) => Console.WriteLine("\n\t" + a + " - " + b + " = " + (a - b));
+        /// <summary>
+        /// Добуток
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static void Mul(double a, double b) => Console.WriteLine("\n\t" + a + " * " + b + " = " + (a * b));
+        /// <summary>
+        /// Частка
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static void Div(double a, double b)
+        {
+            try
+            {
+                if (b == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                else
+                {
+                    // може рахувати для double, але не для int
+                    Console.WriteLine("\n\t" + a + " / " + b + " = " + (a / b));
+                }
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+
+                Console.WriteLine("\n\tАле все таки, правильна відповідь:");
+                if (a > 0)
+                {
+                    Console.WriteLine("\n\t" + a + " / " + b + " = " + double.PositiveInfinity);
+                }
+                else if (a < 0)
+                {
+                    Console.WriteLine("\n\t" + a + " / " + b + " = " + double.NegativeInfinity);
+                }
+                else if (a == 0)
+                {
+                    Console.WriteLine("\n\t" + a + " / " + b + " = " + double.NaN);
+                }
+            }
+        }
+
+    }
+}
---
+++ LesApp0/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp0
+{
+    class Program
+    {
+        /// <summary>
+        /// Делегат арифметичної операції
+        /// </summary>
+        delegate void DelAO(double a, double b);
+
+        static void Main()
+        {
+            // Join Unicode
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // змінні для розрахунку
+            double a, b;
+            // операція для виконання
+            string oper;
+            // доступ до наступної операції
+            bool error = true;
+            // делегат операції
+            DelAO delAO = null;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            // введення
+            Console.Write("\n\tКалькулятор:\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            // введення даних
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("\nВведіть число: ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("a = ");
+            error = double.TryParse(Console.ReadLine().Replace(".", ","), out a);
+            AnaliseOfInputNumber(error);
+
+            // введення операції
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Введіть операцію: ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            oper = Console.ReadLine();
+            error = "+-*/".Contains(oper);
+            //error = oper.Contains("+-*/");
+            AnaliseOfInputNumber(error);
+            // присвоєння операції
+            delAO = ChangeAO(oper);
+
+            // введення даних
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Введіть число: ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("b = ");
+            error = double.TryParse(Console.ReadLine().Replace(".", ","), out b);
+            AnaliseOfInputNumber(error);
+
+            // виведення даних
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"\n\tРезультат: \n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            delAO(a, b);
+
+            // repeat
+            DoExitOrRepeat();
+        }
+
+        static DelAO ChangeAO(string oper)
+        {
+            switch (oper)
+            {
+                case "+":
+                    return Calculator.Add;
+                case "-":
+                    return Calculator.Sub;
+                case "*":
+                    return Calculator.Mul;
+                case "/":
+                    return Calculator.Div;
+                default:
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// Метод виходу або повторення методу Main()
+        /// </summary>
+        static void DoExitOrRepeat()
+        {
+            Console.WriteLine("\n\nСпробувати ще раз: [т, н]");
+            Console.Write("\t");
+            var button = Console.ReadKey(true);
+
+            if ((button.KeyChar.ToString().ToLower() == "т") ||
+                (button.KeyChar.ToString().ToLower() == "n")) // можливо забули переключити розкладку клавіатури
+            {
+                Console.Clear();
+                Main();
+                // без використання рекурсії
+                //Process.Start(Assembly.GetExecutingAssembly().Location);
+                //Environment.Exit(0);
+            }
+            else
+            {
+                // закриває консоль
+                Environment.Exit(0);
+            }
+        }
+
+        /// <summary>
+        /// Умова коли невірно введені дані
+        /// </summary>
+        /// <param name="res"></param>
+        static void AnaliseOfInputNumber(bool res)
+        {
+            if (!res)
+            {
+                Console.WriteLine("\nНевірно введені дані!");
+                DoExitOrRepeat();
+            }
+        }
+    }
+}
